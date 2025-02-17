0c4833feafb714b23c8b5fd3b205187ea0cf5715
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2025-02-17 17:43:16 +0100

    Update help.js

--- commands/application/help.js
+++ commands/application/help.js
@@ -9,17 +9,17 @@ module.exports = {
     const embed = new MessageEmbed()
           .setColor('#0x1a2c78')
           .setTitle('Need Help?')
-          .setAuthor({ name: 'KOF02UM FrameBot', iconURL: 'https://cdn.discordapp.com/icons/630700639382405120/2f0abc591122b2bd038650bee6868faa.webp?size=240', url: 'https://discord.gg/8JNXHxf' })
+          .setAuthor({ name: 'KOFXIII FrameBot', iconURL: 'https://cdn.discordapp.com/icons/284354385687871488/23c6ea888ff2fec1dea94da791b9265a.webp?size=128', url: 'https://discord.gg/tNgSuGJ' })
           .addFields(
             { name: 'Getting started', value: 'The bot provides a "move per command" response where you get to ask for information of a certain move of a certain character individually per request. The bot uses autocomplete, so please keep typing to filter the results to your needs. The bot has a **/frames** slash command which accept 2 arguments:', inline: false },
             { name: '\u200B', value: '\u200B' },
-            { name: 'Character', value: 'The **character** which is a case insensitive string (e.g. vanessa, Chris, iori)', inline: false },
+            { name: 'Character', value: 'The **character** which is a case insensitive string (e.g. takuma, Clark, iori)', inline: false },
             { name: 'Move', value: 'The **move** name or input which is a case insensitive string (e.g. crouchA, psycho ball)', inline: false },
             { name: '\u200B', value: '\u200B' },
             { name: 'Demo', value: 'The following is a visual representation of how the bot works:', inline: false },
           )
           .setImage('https://media.giphy.com/media/LrqUuAZB2E3hGvHtOH/giphy.gif')
-          .setFooter({ text: 'Got feedback? Join the 02UM server: https://discord.gg/8JNXHxf', iconURL: 'https://cdn.iconscout.com/icon/free/png-128/discord-3-569463.png' });
+          .setFooter({ text: 'Got feedback? Join the XIII server: https://discord.gg/tNgSuGJ', iconURL: 'https://cdn.iconscout.com/icon/free/png-128/discord-3-569463.png' });
         return interaction.reply({embeds: [embed]});
   },
 };
