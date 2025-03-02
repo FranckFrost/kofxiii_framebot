af096554d3aca912be07805421334f1dd15b7959
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2025-03-02 22:04:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,22 +21,34 @@ for (const file of guildCommandFiles) {
 }
 
 let json = null
-let characters = []
+let characters = [], json_characters = [];
 client.once('ready', () => {
   json = fs.readFileSync("./assets/framedataxiii.json", 'utf8');
   json = JSON.parse(json);
   Object.keys(json).forEach(function (key) {
-    characters.push(key);
+    json_characters.push(key);
   })
   console.log('Ready!');
 });
 client.on('interactionCreate', async autocomplete => {
 	if (!autocomplete.isAutocomplete()) return;
   // console.log(autocomplete.commandName)
-	if (autocomplete.commandName === 'embed' || autocomplete.commandName === 'frames') {
+	if (autocomplete.commandName === 'embed' || autocomplete.commandName === 'frames' || autocomplete.commandName === 'cargo') {
     let currentOption = autocomplete.options.getFocused(true);
     let currentName = currentOption.name;
     let currentValue = currentOption.value;
+    characters = json_characters;
+
+    if (autocomplete.commandName === 'cargo') {
+	    let cargo_characters = []
+	    const url_char = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_KOFXIII%2C&&fields=MoveData_KOFXIII.chara%2C&&group+by=MoveData_KOFXIII.chara&order+by=&limit=100&format=json"
+	    const response_char = await fetch(url_char);
+	    const cargo_char = await response_char.json();
+	    for (let x in cargo_char) {
+		    cargo_characters.push(cargo_char[x]["chara"])
+	    }
+	    characters = cargo_characters;
+    }
 
     const options = [];
     if (currentName === "character") {
@@ -56,38 +68,65 @@ client.on('interactionCreate', async autocomplete => {
     // If move is focused 
     if (currentName === "move" && character !== "") {
       let moveObj = {}
-	if (json[character] === undefined) {
-	    // Capitilize first letter(s) of character name.
+	    // Capitilize first letter(s) of char name.
 	    let a = (character.split(' ')[1]!==undefined) ? ' ' + character.split(' ')[1].charAt(0).toUpperCase() + character.split(' ')[1].slice(1) : ""
 	    let char = character.split(' ')[0].charAt(0).toUpperCase() + character.split(' ')[0].slice(1) + a;
-	    // Temp: validate extra names.
+	    // Validate extra names.
 	    character = getCharacter(char)
-	}
-      if (json[character] === undefined) {
-        moveObj["name"] = 'Moves not found for specified character, try another character';
-        moveObj["value"] = 'Moves not found for specified character, try another character';
-        options.push(moveObj);
-      } else {
-        let moves = [] 
-        Object.keys(json[character]).forEach(function (key) {
-          moves.push(key);
-        })
-        // console.log(moves)
-        // console.log('currval ' + currentValue)
-        moves.forEach((move) => {
-          if (move.toLowerCase().includes(currentValue.toLowerCase())) {
-            moveObj = {}
-            moveObj["name"] = move;
-            moveObj["value"] = move;
-            // console.log(move)
-            if (options.length < 25) {
-              options.push(moveObj);
-            }
-          }
-        }) 
-      }
+	    if (autocomplete.commandName === 'cargo') {
+		    if (!characters.includes(character)) {
+			    moveObj["name"] = 'No cargo data available for specified character. Gather framedata with /frames instead.';
+                            moveObj["value"] = 'No cargo data available for specified character. Gather framedata with /frames instead.';
+                            options.push(moveObj);
+		    } else {
+			    let move = "";
+			    let val = "";
+			    const url_moves = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_KOFXIII%2C&&fields=MoveData_KOFXIII.input%2C+MoveData_KOFXIII.input2%2C+MoveData_KOFXIII.name%2C+MoveData_KOFXIII.moveId%2C&where=chara%3D%22"+encodeURIComponent(character)+"%22&order+by=MoveData_KOFXIII._ID+ASC&limit=100&format=json"
+			    const response_moves = await fetch(url_moves);
+			    const cargo_moves = await response_moves.json();
+			    for (let x in cargo_moves) {
+				    move = cargo_moves[x]["name"]
+				    if (cargo_moves[x]["input"] !== null) {
+					    move = cargo_moves[x]["name"] + " (" + cargo_moves[x]["input"] + ")"
+					    if (cargo_moves[x]["input2"] !== null && cargo_moves[x]["input"] !== cargo_moves[x]["input2"]) {
+						    move = cargo_moves[x]["name"] + " ([" + cargo_moves[x]["input"] + "] / [" + cargo_moves[x]["input2"] + "])"
+						    val = cargo_moves[x]["moveId"] + "??" + move
+						    if (val.length > 100) {   // choice character limit of 100
+							    move = cargo_moves[x]["name"] + " ([" + cargo_moves[x]["input"].trim() + "] / [" + cargo_moves[x]["input2"].trim() + "])"; 
+						    }
+					    }
+				    }
+				    if (move.toLowerCase().includes(currentValue.toLowerCase())) {
+					    moveObj = {}
+					    moveObj["name"] = move;
+					    moveObj["value"] = cargo_moves[x]["moveId"] + "??" + move;
+					    if (options.length < 25) options.push(moveObj);
+				    }
+			    }
+					  }
+	    } else {
+		    if (json[character] === undefined) {
+			    moveObj["name"] = 'Moves not found for specified character, try another character';
+			    moveObj["value"] = 'Moves not found for specified character, try another character';
+			    options.push(moveObj);
+		    } else {
+			    let moves = [];
+			    Object.keys(json[character]).forEach(function (key) {
+				    moves.push(key);
+			    })
+			    moves.forEach((move) => {
+				    if (move.toLowerCase().includes(currentValue.toLowerCase())) {
+					    moveObj = {}
+					    moveObj["name"] = move;
+					    moveObj["value"] = move;
+					    // console.log(move)
+					    if (options.length < 25) options.push(moveObj);
+				    }
+			    })
+					  }
+	    }
     }
-		await autocomplete.respond(options);
+	    await autocomplete.respond(options);
 	}
 });
 client.on('interactionCreate', async interaction => {
