7becbba934ea65d2d8f4490a138adb4e60f3abc2
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2025-05-26 01:33:49 +0200

    Update cargo.js

--- commands/application/cargo.js
+++ commands/application/cargo.js
@@ -147,6 +147,14 @@ module.exports = {
       }
   },
   getCharacter: function(character) {
+    // Capitilize first letters of each word of the char name.
+    let words = character.split(' ')
+    for (let i in words) {
+	    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+    }
+    let char = words.join(' ');
+	
+    // Validate extra names.
     const chart = {
       'Andy': 'Andy Bogard',
       'Ash': 'Ash Crimson',
@@ -183,10 +191,10 @@ module.exports = {
       'Terry': 'Terry Bogard',
       'Yuri': 'Yuri Sakazaki'
     };
-    if (chart[character] === undefined) {
-      return character;
+    if (chart[char] === undefined) {
+      return char;
     }
-    return chart[character];
+    return chart[char];
   },
   getHyperLink: function(str,inv) {
     if (inv && str === null) return 'No recorded invincibility.'; // no invuln found
