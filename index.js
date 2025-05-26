63b3b9c655cd8a88a24a0289c6d3ff961aa71b37
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2025-05-26 05:42:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -177,6 +177,14 @@ const token = process.env['DISCORD_TOKEN']
 client.login(token);
 
 function getCharacter(character) {
+    // Capitilize first letters of each word of the char name.
+    let words = character.split(' ')
+    for (let i in words) {
+	    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+    }
+    let char = words.join(' ');
+	
+    // Validate extra names.
     const chart = {
       'Andy': 'Andy Bogard',
       'Ash': 'Ash Crimson',
@@ -213,8 +221,8 @@ function getCharacter(character) {
       'Terry': 'Terry Bogard',
       'Yuri': 'Yuri Sakazaki'
     };
-    if (chart[character] === undefined) {
-      return character;
+    if (chart[char] === undefined) {
+      return char;
     }
-    return chart[character];
+    return chart[char];
 };
